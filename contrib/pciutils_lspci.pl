3c576390b3d32bdfe35e384d66f107dc8c719f0c
ccsgac <ccsgac@csc.warwick.ac.uk>
2024-09-02 15:47:42 +0100

    Move temporary bin/* to contrib/ ; Update MANIFEST

--- contrib/pciutils_lspci.pl
+++ contrib/pciutils_lspci.pl
@@ -5,12 +5,14 @@ use warnings;
 use feature qw(say);
 
 use Data::Dumper;
+$Data::Dumper::Sortkeys = 1;
+$Data::Dumper::Terse = 1;
+$Data::Dumper::Indent = 1;
+
 
 use PCIUTILS::LSPCI;
 
 my $obj = PCIUTILS::LSPCI->new;
 my $lspci = $obj->get_lspci;
 
-for my $device ( sort keys %$lspci ) {
-    say "[$device]";
-}
+print Dumper $lspci;
